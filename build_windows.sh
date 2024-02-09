f608ad02f8f4ffb11b64a3087ba060dbbd8e73a0
doongjohn <doongjohn@gmail.com>
2024-02-09 12:44:44 +0900

    update

--- build_windows.sh
+++ build_windows.sh
@@ -2,7 +2,7 @@
 
 COMPILER="$PWD/zigcc"
 LINKER="$PWD/zigcc"
-OPTIONS="-target x86_64-windows"
+OPTIONS="-target x86_64-windows -Doptimize=ReleaseFast"
 OUTPUT="simpleclicker.exe"
 
 nim c --cc:clang --clang.exe="$COMPILER" --clang.linkerexe="$LINKER" --passC:"$OPTIONS" --passL:"$OPTIONS" --os:windows --forceBuild:on -d:release -o:"$OUTPUT" src/main.nim
