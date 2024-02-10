f4c6f1f34f2119f1e1d7055f8aad3040c568dec5
doongjohn <doongjohn@gmail.com>
2024-02-10 21:34:05 +0900

    update

--- build_windows.sh
+++ build_windows.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-COMPILER="$PWD/zigcc"
-LINKER="$PWD/zigcc"
-OPTIONS="-target x86_64-windows -Doptimize=ReleaseFast"
+# zig cc without pdb
+# https://ziggit.dev/t/how-to-use-zig-cc-without-generating-a-pdb-file/2873
+
+ZIGCC="$PWD/zigcc"
+ZIGCC_OPTION="--target=x86_64-windows -Doptimize=ReleaseSmall -s"
 OUTPUT="simpleclicker.exe"
 
-nim c --cc:clang --clang.exe="$COMPILER" --clang.linkerexe="$LINKER" --passC:"$OPTIONS" --passL:"$OPTIONS" --os:windows --forceBuild:on -d:release -o:"$OUTPUT" src/main.nim
+nim c \
+  --cc:clang \
+  --clang.exe="$ZIGCC" \
+  --clang.linkerexe="$ZIGCC" \
+  --passC:"$ZIGCC_OPTION" \
+  --passL:"$ZIGCC_OPTION" \
+  --os:windows \
+  --opt:size \
+  -d:release \
+  -o:"$OUTPUT" \
+  "$@" \
+  src/main.nim
